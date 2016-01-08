d1a6732186c6a907eb38e4cd885034569b7ba202
mitchwongho <oreomitch@gmail.com>
2016-01-08 12:34:46 +0200

    travis script testing 1

--- scripts/travis-sonatype-publish.sh
+++ scripts/travis-sonatype-publish.sh
@@ -3,6 +3,8 @@
 # Based closely on material from
 # http://benlimmer.com/2014/01/04/automatically-publish-to-sonatype-with-gradle-and-travis-ci.
 
+echo -e "(sonatype_publish=$SONATYPE_PUBLISH)"
+
 if [ "$SONATYPE_PUBLISH" == "true" ] && [ "$TRAVIS_PULL_REQUEST" == "false" ] && [ "$TRAVIS_BRANCH" == "$SONATYPE_PUBLISH_BRANCH" ]; then
   echo -e "Starting publish to Sonatype...\n"
 
